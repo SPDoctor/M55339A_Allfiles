85828f9f06df1299dda9de0512831c867b3b4fe4
SURFACE7PLUS\BillA <BillA@flosim.com>
2024-09-05 13:36:52 +0100

    Removed redundant serialization code for custom exception which was raising warnings

--- Mod11/Labfiles/Starter/Exercise 1/Grades.DataModel/ClassFullException.cs
+++ Mod11/Labfiles/Starter/Exercise 1/Grades.DataModel/ClassFullException.cs
@@ -2,7 +2,6 @@
 
 namespace Grades.DataModel
 {
-    [Serializable]
     public class ClassFullException : Exception
     {
         // Custom data: the name of the class that is full
@@ -44,28 +43,5 @@ namespace Grades.DataModel
         {
             _className = cls;
         }
-
-        #region Code provided to handle deserialization of a custom exception
-        // Constructor for deserializing a ClassFullException object
-        // The _className field contains custom data, so it must be handled explicitly
-        // The details are outside the scope of this lab
-        protected ClassFullException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-            // Populate the _className member from the deserialization stream 
-            _className = info.GetString("ClassName");
-        }
-
-
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            if (info == null)
-            {
-                throw new ArgumentException("info");
-            }
-            base.GetObjectData(info, context);
-            info.AddValue("ClassName", _className, typeof(string));
-        }
-        #endregion
     }
 }
